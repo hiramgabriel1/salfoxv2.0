c1f0a0c4585e65a3ed8fd07ad7a8dd7ffaf7468b
itsrusty <rusty3031@gmail.com>
2023-08-25 23:10:42 -0700

    add controllers to posts

--- src/controllers/posts/posts.js
+++ src/controllers/posts/posts.js
@@ -1,9 +1,57 @@
-import posts from "../../models/Posts.js"
+import posts from "../../models/Posts.js";
 
+// TODO: create a post
 export const createPost = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        throw error(error)
+  try {
+    // ? obtener las publicaciones
+    const { titlePost, description } = req.body;
+
+    const dataPost = {
+      titlePost,
+      description,
+    };
+
+    // ? save in database
+    const saveData = await posts.create(dataPost);
+
+    if (saveData.satuts == 200) {
+      res.json({ message: dataPost });
     }
-}
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+// TODO: filter posts by title
+export const filterPosts = async (req, res) => {
+  try {
+    const { titlePost } = req.params;
+
+    const filterPostByTitle = await posts.find({
+      $text: { $search: titlePost },
+    });
+
+    // validate
+    if (filterPostByTitle) {
+      res.status(200).json({ message: filterPostByTitle });
+    } else if (!filterPostByTitle || filterPostByTitle.length === 0) {
+      res.status(404).json({ message: "no encontrado" });
+    }
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+// TODO: show all posts
+export const showAllPosts = async (req, res) => {
+  try {
+    const dataPost = await posts.find();
+    if (dataPost) {
+      res.json({ message: dataPost });
+    } else {
+      res.status(500).json({ errorMessage: "Error del servidor" });
+    }
+  } catch (error) {
+    throw new Error(error);
+  }
+};
