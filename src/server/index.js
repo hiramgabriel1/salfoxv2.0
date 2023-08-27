42a6bb39f3c0af4fe296b9f6cb1c655530619907
itsrusty <rusty3031@gmail.com>
2023-08-26 19:59:09 -0700

    add routes

--- src/server/index.js
+++ src/server/index.js
@@ -1,29 +1,36 @@
-import express from "express"
-import dotenv from "dotenv"
-import morgan from "morgan"
-import colors from "colors"
-import cors from "cors"
-import userRoute from "../routes/user.js"
-import connectionDB from "../config/connectionDB.js"
-import postsRoute from "../routes/posts/posts.js"
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import colors from "colors";
+import cors from "cors";
+import fileUpload from "express-fileupload";
+import { PORT } from "../config/keys.js";
+import userRoute from "../routes/user.js";
+import connectionDB from "../config/mongoose.config.js";
+import postsRoute from "../routes/posts/posts.js";
 
 // TODO: initialization to DB
-connectionDB()
+connectionDB();
 
-const app = express()
-const PORT = process.env.PORT || 3000
+const app = express();
 
 // TODO: connect to PORT
-dotenv.config()
+dotenv.config();
 
 // TODO: middlewares
-app.use(express.json())
-app.use(morgan("dev"))
-app.use(cors())
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/uploads/",
+  })
+);
 
 // TODO: routes
-app.use("/auth", userRoute)
-app.use("/post", postsRoute)
+app.use("/auth", userRoute);
+app.use("/post", postsRoute);
 
-// TODO: PORT listening 
-app.listen(PORT)
+// TODO: PORT listening
+app.listen(PORT);
