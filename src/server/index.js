5f1efa35686981071da1dd1b8168f965f3244032
itsrusty <rusty3031@gmail.com>
2023-08-27 00:34:04 -0700

    edit index.js

--- src/server/index.js
+++ src/server/index.js
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import colors from "colors";
 import cors from "cors";
-import fileUpload from "express-fileupload";
 import { PORT } from "../config/keys.js";
 import userRoute from "../routes/user.js";
 import connectionDB from "../config/mongoose.config.js";
@@ -21,12 +20,11 @@ dotenv.config();
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
-app.use(
-  fileUpload({
-    useTempFiles: true,
-    tempFileDir: "/uploads/",
-  })
-);
+
+// ! error handling
+// app.use((req, res) => {
+//   res.status(404).send("Recurso no encontrado");
+// });
 
 // TODO: routes
 app.use("/auth", userRoute);
