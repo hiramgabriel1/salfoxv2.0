7664e719a063d025ac15ff2aaed55f976edd00dc
itsrusty <rusty3031@gmail.com>
2023-08-26 19:58:45 -0700

    add METHODS

--- src/config/cloudinary.config.js
+++ src/config/cloudinary.config.js
@@ -1,1 +1,22 @@
-import { v2 as cloudinary } from "cloudinary"
+import { v2 as cloudinary } from "cloudinary";
+import { CLOUDINARY_KEY, CLOUDINARY_NAME, CLOUDINARY_SECRET } from "./keys.js";
+
+// & config object
+
+cloudinary.config({
+  cloud_name: CLOUDINARY_NAME,
+  api_key: CLOUDINARY_KEY,
+  api_secret: CLOUDINARY_SECRET,
+});
+
+// & upload image METHOD
+export const uploadImage = async (filePath) => {
+  return await cloudinary.uploader.upload(filePath, {
+    folder: "image",
+  });
+};
+
+// & delete image METHOD
+export const deleteImages = async (filePath) => {
+  return await cloudinary.uploader.destroy(publicId);
+};
